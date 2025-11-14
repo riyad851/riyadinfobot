22318b7e5503f4a5ef301bebadbbe881e598d46a
riyad851 <riyadalhasan10@gmail.com>
2025-11-14 14:56:28 +0600

    Simplify start.sh by removing comments and pip upgrade
    Removed comments and pip upgrade step from start script.

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# =============================
-# Riyad FF Info Bot Start Script
-# =============================
-
-# Upgrade pip and install dependencies
-pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
-
-# Run the bot
 python main.py
